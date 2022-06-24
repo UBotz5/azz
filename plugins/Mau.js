2c9e7291e0d274de05a24e4b5fdc65322064478c
UtkkBot <108014592+UtkkBot@users.noreply.github.com>
2022-06-24 21:21:56 +0700

    Add files via upload

--- plugins/Mau.js
+++ plugins/Mau.js
@@ -2,13 +2,13 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
   Gelud : *_Mau_* 
-*Lah ayo gelud anyingg, maju lu asu bgsddd sini asuu
-Serlok cok serlok sini gelud kita. Asuu kauuu 😠*
+  
+Lah ayo gelud anyingg, maju lu asu bgsddd sini asuu
+Serlok cok serlok sini gelud kita. Asuu kauuu 😠
 `.trim()
 conn.sendButtonImg(m.chat, await (await fetch(fla + 'Gelud')).buffer(), ext, 'Pukul', '', '', m)
 
        }
        
-handler.customPrefix = /^(mau)$/i
-handler.command = new RegExp
+handler.command = /^(mau)$/i
 module.exports = handler
--- plugins/Nggak.js
+++ plugins/Nggak.js
@@ -2,13 +2,13 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
   Gelud : *_Gak Mau_* 
-*Heheh anak baikk, bagus sayangg
-Lopyu dah buat kamu ><*
+  
+Heheh anak baikk, bagus sayangg
+Lopyu dah buat kamu ><
 `.trim()
 conn.sendButtonImg(m.chat, await (await fetch(fla + 'Anak Baik')).buffer(), ext, 'Nggak', '😇', '😇', m)
 
        }
        
-handler.customPrefix = /^(nggak)$/i
-handler.command = new RegExp
+handler.command = /^(nggak)$/i
 module.exports = handler
