9f802d0717e19fd484292101458fbd7715c008c4
AzzBotz <108014592+UtkkBot@users.noreply.github.com>
2022-06-26 12:20:30 +0700

    Add files via upload

--- plugins/islam-doaharian.js
+++ plugins/islam-doaharian.js
@@ -1,23 +1,17 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   await m.reply(global.wait)
-  let res = await fetch(global.API('xteam','/religi/doaharian', {}, 'APIKEY'))
-  let json = await res.json()
-  if (res.status != 200) throw json
-  if (json.result.error) throw json.result.message
-  let {
-    title,
-    latin,
-    arabic,
-    translation
-  } = json.result
+  let res = await fetch('https://islamic-api-zhirrr.vercel.app/api/doaharian')
+  let son = await res.json()
+  shyi = son.data
+  let json = shyi[Math.floor(Math.random() * shyi.length)]
   let caption = `
-*「 Doa Harian 」*
-${title}
-${arabic}
-${latin}
-Artinya:
-_"${translation}"_
+*─────────[ Doa Harian ]─────────*
+*${json.title}*
+${json.arabic}
+${json.latin}
+
+*Artinya:*
+_"${json.translation}"_
 `.trim()
   await m.reply(caption)
 }
