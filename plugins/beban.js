cefd7be338ea68d3b6c3f0812e4625593f24f554
AzzBotz <108014592+UtkkBot@users.noreply.github.com>
2022-06-26 11:44:54 +0700

    Add files via upload

--- plugins/beban.js
+++ plugins/beban.js
@@ -1,11 +1,16 @@
 let handler = async (m, { conn }) => {
-let caption = `𝖩𝖺𝗇𝗀𝖺𝗇 𝗅𝗎𝗉𝖺 𝗂𝗇𝗍𝗋𝗈 𝗄𝖺𝗄 🤗
-	
-╭ 𝖭𝖺𝗆𝖺 :
-├ 𝖴𝗆𝗎𝗋 :
-├ 𝖦𝖾𝗇𝖽𝖾𝗋 :
-├ 𝖠𝗌𝗄𝗈𝗍 :
-╰❒ 𝖯𝖺𝗍𝗎𝗁𝗂 𝗋𝗎𝗅𝖾𝗌 𝗀𝗋𝗎𝗉 𝗒𝖺 𝗄𝖺k ~`
+let caption = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
+│       *「 Intro 」*
+│ *Nama     :* 
+│ *Gender   :* 
+│ *Umur      :* 
+│ *Hobby    :* 
+│ *Kelas      :* 
+│ *Asal         :* 
+│ *Agama    :* 
+ |  *Status     :* 
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`
 
 conn.sendBut( m.chat, caption, ``, `OKE KAK><`,  m)
 
