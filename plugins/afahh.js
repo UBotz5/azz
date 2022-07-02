98a0ce667e3f77c095cd3d7ffcad68cdc6b28601
UBotz5 <107531148+UBotz5@users.noreply.github.com>
2022-07-02 23:20:30 +0700

    Add files via upload

---
+++ plugins/afahh.js
@@ -0,0 +1,23 @@
+let handler = m => m
+
+let linkRegex = /(p|oi|oy|hi|hai|hy|hay)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupToxic = linkRegex.exec(m.text)
+
+  if (chat.antiToxic && isGroupToxic) {
+    m.reply('Apa Sayy' + readMore + 'tonnn')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
